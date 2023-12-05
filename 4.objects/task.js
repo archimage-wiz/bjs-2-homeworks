981105911d3f9177d9e3266dd8f0254ac5c9d05b
archimage-wiz <archimage@yandex.ru>
2023-12-05 18:15:19 +0300

    Tsk 4 ready.
    Changes to be committed:
    	new file:   .gitignore
    	modified:   task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,48 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+    this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (this.marks) {
+        this.marks.push(...marksToAdd);
+    }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+    }
+    return this.marks.reduce((a, b) => a + b) / this.marks.length;
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+    if (this.marks) {
+        delete this.marks;
+    }
+    if (this.subject) {
+        delete this.subject;
+    }
+    this.excluded = reason;
+};
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+// {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude("плохая учёба");
+student2.addMarks(4, 5, 4, 5);
+console.log(student2);
+// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
+
