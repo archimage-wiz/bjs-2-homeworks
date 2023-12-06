bc22635e763e2b0235ecafe70a1cea5cbafa7b1f
archimage-wiz <archimage@yandex.ru>
2023-12-06 17:05:36 +0300

    Classes task rdy.
    	new file:   5.classes/.gitignore
    	modified:   5.classes/task.js

---
+++ 5.classes/task.js
@@ -0,0 +1,121 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this._state = 100;
+        this.type = null;
+    }
+    fix() {
+        this._state *= 1.5;
+        if (this._state > 100) {
+            this._state = 100;
+        }
+    }
+
+    /**
+     * @param {number} value
+     */
+    set state(value) {
+        if (value > 100) {
+            this._state = 100;
+        } else if (value < 0) {
+            this._state = 0;
+        } else {
+            this._state = value;
+        }
+    }
+    get state() {
+        return this._state;
+    }
+}
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "magazine";
+    }
+}
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = "book";
+    }
+}
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "novel";
+    }
+}
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "fantastic";
+    }
+}
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "detective";
+    }
+}
+
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+    addBook(book) {
+        if (book.state > 30) {
+            this.books.push(book);
+        }
+    }
+    findBookBy(type, value) {
+        for (const book of this.books) {
+            if (book[type] === value) {
+                return book;
+            }
+        }
+        return null;
+    }
+    giveBookByName(bookName) {
+        for (const book of this.books) {
+            if (book.name === bookName) {
+                this.books.splice(this.books.indexOf(book), 1);
+                return book;
+            }
+        }
+        return null;
+    }
+}
+
+const library = new Library("Библиотека имени Ленина");
+
+library.addBook(
+    new DetectiveBook(
+        "Артур Конан Дойл",
+        "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+        2019,
+        1008
+    )
+);
+library.addBook(
+    new FantasticBook(
+        "Аркадий и Борис Стругацкие",
+        "Пикник на обочине",
+        1972,
+        168
+    )
+);
+
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+
+console.log(library.findBookBy("name", "Властелин колец")); //null
+console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
+
+console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
+library.giveBookByName("Машина времени");
+console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
